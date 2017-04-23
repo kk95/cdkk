9fa581d7d11cdbde7bbd7b10d5d8ffda333e169b
christianrdumas <christianrdumas11@gmail.com>
2017-04-23 19:35:43 -0400

    Pushing some command line implementation stuff

--- project.rkt
+++ project.rkt
@@ -1,3 +1,32 @@
 #lang racket
 
+(require csv-reading)
+(require net/url)
+(require xml)
+(require html)
 
+(print "Command Line:")
+
+(newline)
+
+
+
+(define (get-stock-values stksymbol)
+  (map cadr (cdadr (csv->sxml (pcdata-string (car (read-html-as-xml (get-pure-port (string->url (string-append "http://download.finance.yahoo.com/d/quotes.csv?s=" (symbol->string stksymbol) "&f=po"))))))))))
+
+(define (stkinterp invalues)
+  (cond [(eq? (car invalues) 'add_stock) (make-stock (cadr invalues))]
+               [(eq? (car invalues) 'help) (begin
+                                             (print "add_stock STOCKNAME")
+                                             (newline))]))
+         ;(stkinterp (read))))
+
+(define (make-stock symbolname)
+  (let ((stocksymbol symbolname)
+        (stockvals (get-stock-values symbolname)))
+        (lambda (command)
+          (cond [(eq? command 'symbol) stocksymbol]
+                [(eq? command 'open) (cadr stockvals)]
+                [(eq? command 'close) (car stockvals)]))))
+
+(define method (stkinterp (read)))
